98b7953eb143790bb0d23fc9dceba52398f0a017
Madhukara <madhukara@astrome.co>
2020-02-22 18:58:17 +0530

    Added comments

--- selection_sort.py
+++ selection_sort.py
@@ -2,16 +2,20 @@ from random import randint
 
 n = randint(9000, 11000) # n lies between 9000 and 11000 (both included)
 array = [None] * n # creates a list of length n 
-min_value = randint(1, 100)
-max_value = 5 * n
+min_value = randint(1, 100) # Lower bound for the values in the array
+max_value = 5 * n # Upper bound for the values in the array
 
-def generate_list(min_value, max_value):
+def generate_list(min_value, max_value): 
+    """Returns a list with n randomly generated integers. 
+       Range: min_value to max_value"""
     for i in range(n):
         num = randint(min_value, max_value)
         array[i] = num
     return array
 
 def selection_sort(x):
+    """Returns the list with elements sorted in increasing order.
+       Algorithm: Selection sort"""
     for i in range(0, n-1):
         mim = x[i]
         for j in range(i+1, n):
@@ -24,6 +28,9 @@ def selection_sort(x):
     return x
 
 def verify_sort(x):
+    """Returns boolean.
+       True if the list elements are sorted in increasing order.
+       False in other cases."""
     for i in range(0, n-1):
         if x[i] > x[i+1]:
             return False

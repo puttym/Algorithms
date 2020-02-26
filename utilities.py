b1551bf30e1cebd9afa22e074a90c4610267ead5
Madhukara <madhukara@astrome.co>
2020-02-26 11:00:15 +0530

    Added a function to generate strings

--- utilities.py
+++ utilities.py
@@ -1,26 +1,35 @@
 from random import randint
 
 def set_constants():
-    n = randint(9000, 11000)
-    array = [None] * n
+    array_length = randint(9000, 11000)
+    array = [None] * array_length 
     min_val = randint(1, 100)
-    max_val = n * 5
-    return n, array, min_val, max_val
+    max_val = array_length * 5
+    return array_length, array, min_val, max_val
 
-def generate_list(n, array, min_val, max_val):
+def generate_int_list(array_length, array, min_val, max_val):
     """Returns a list with n randomly generated integers.
        Range: From min_val to max_val"""
-    for i in range(n):
+    for i in range(array_length):
         num = randint(min_val, max_val)
         array[i] = num
     return array
 
-def verify_sort(n, x):
+def generate_str_list(string_length, array_length, array):
+    for i in range(array_length):
+        p = ''.join(random.choices(string.ascii_uppercase + string.digits, k = string_length))
+        array[i] = p
+    return array
+    
+
+
+
+def verify_sort(array_length, x):
     """Returns boolean.
        Returns True if the list elements are sorted in increasing order
        Returns False for other cases.
     """
-    for i in range(0, n-1):
+    for i in range(0, array_length-1):
         if x[i] > x[i+1]:
             result = False
         else:
@@ -28,5 +37,5 @@ def verify_sort(n, x):
     return result 
 
 if __name__ == "__main__":
-    n, array, min_val, max_val = set_constants()
-    x = generate_list(n, array, min_val, max_val)
+    array_length, array, min_val, max_val = set_constants()
+    x = generate_list(array_length, array, min_val, max_val)
